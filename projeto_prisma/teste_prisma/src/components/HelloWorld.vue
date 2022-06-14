f964f81ff445f729f01454060d6bfd596f460769
André Fragas <andrefragas14@gmail.com>
2022-06-14 00:23:28 -0300

    Alteração de requisição

--- projeto_prisma/teste_prisma/src/components/HelloWorld.vue
+++ projeto_prisma/teste_prisma/src/components/HelloWorld.vue
@@ -65,29 +65,13 @@ export default {
         //alert("O campo de e-mail está em branco!");
         return "O campo de e-mail está em branco!";
       }
-      //var raw = JSON.stringify(this.estado);
-      //var raw2 = JSON.stringify(this.email);
 
       let endpoint = "https://localhost:44311/api/projeto?email="+this.email+"&estado="+this.estado;
-      request.open("GET",endpoint)
+      let request = new XMLHttpRequest();
+      request.open("Get", endpoint);
       request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      request.send(JSON.stringify(this.email, this.estado));
+      request.send();
 
-      //var myHeaders = new Headers();
-      //myHeaders.append("Content-Type", "application/json");
-
-      //var requestOptions = {
-      //  method: 'Post',
-      //  body: raw, raw2,
-      //  headers: myHeaders,
-      //  redirect: 'follow'
-      //};
-
-      //fetch("https://localhost:7036/api/projeto", requestOptions)
-      //  .then(response => response.text())
-      //  .then(result => console.log(result))
-      //  .catch(error => console.log('error', error));
-      
       this.estado = "";
       this.email = "";
     }
